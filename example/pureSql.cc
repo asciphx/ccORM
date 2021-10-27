7de41db346eca7d83574a7e3ed01d8890e887cd1
asciphx <42582375+asciphx@users.noreply.github.com>
2021-10-27 09:53:46 +0800

    fix

--- example/pureSql.cc
+++ example/pureSql.cc
@@ -3,8 +3,8 @@
 auto d = D_mysql(); auto d1 = D_sqlite("test.db"); auto d2 = D_pgsql();
 using namespace crow; using namespace std;
 int main(int argc, char* argv[]) {
-  std::locale::global(std::locale(u8"en_US.UTF8"));//设置系统为utf8编码
-  json js = json::parse(u8"{ \"state\": true, \"pi\": 3.1415926, \"name\": \"中文测试\" }");
+  std::locale::global(std::locale("en_US.UTF8"));//设置系统为utf8编码
+  json js = json::parse("{ \"state\": true, \"pi\": 3.1415926, \"name\": \"中文测试\" }");
   std::cout<<js<<std::endl;
   Timer t; int i = 0; t.setInterval([&i]() { cout << "After " << ++i << "s\n"; }, 1000);
   auto q = d.conn();
@@ -17,11 +17,11 @@ int main(int argc, char* argv[]) {
   q("DROP table if exists users_test;");
   q("CREATE TABLE if not exists users_test (id int,name TEXT,age int);");
   auto add = q.query(string("INSERT into users_test(id, name, age) values (?,?,?);"));
-  add(1, u8"代码哥", 44);
-  add(2, u8"攻城师", 23);
-  add(3, u8"程序员", 21); //使用方法： ./main "SELECT * from users_test"
-  q.query(string("INSERT into users_test(id, name, age) values (?,?,?);"))(4, u8"编程王", 32);
-  EXPECT_EQUAL(make_tuple(u8"程序员", 21), (q("select name, age from users_test where id = 3;").template r__<string, int>()));
+  add(1, "代码哥", 44);
+  add(2, "攻城师", 23);
+  add(3, "程序员", 21); //使用方法： ./main "SELECT * from users_test"
+  q.query(string("INSERT into users_test(id, name, age) values (?,?,?);"))(4, "编程王", 32);
+  EXPECT_EQUAL(make_tuple("程序员", 21), (q("select name, age from users_test where id = 3;").template r__<string, int>()));
   //generic_sql_tests(d);//测试 mysql
   //generic_sql_tests(d1);//测试 sqlite
   //generic_sql_tests(d2);//测试 pgsql
